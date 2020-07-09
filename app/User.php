331fd9bf6d58faf8bbac58c471dbc5f0f3e8ba75
GabrielIbujes <gaboibujes@gmail.com>
2020-07-09 17:00:18 -0500

    #3-Creacion de seeders

--- app/User.php
+++ app/User.php
@@ -2,13 +2,11 @@
 
 namespace App;
 
-
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
 
-
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
     use Notifiable;
 
@@ -38,13 +36,4 @@ class User extends Authenticatable implements JWTSubject
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 }
